fe98259b0da51c2e849227714c9cdb1b2d481a13
Priyanka.Rangole <priyanka.rangole@sunbeaminfo.com>
2024-04-06 17:52:48 +0530

    updated@apr6

--- Controllers/LogSheetController.cs
+++ Controllers/LogSheetController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using WebAPI.DatabaseContext;
 using WebAPI.DTO;
-using WebAPI.Model;
 using WebAPI.Service;
 
 namespace WebAPI.Controllers
@@ -15,58 +14,14 @@ namespace WebAPI.Controllers
         ProjectDbContext _db;
         public LogsheetController(ProjectDbContext db)
         {
-            _db = db;
-            
+                _db = db;
+            service = new CourseService();
         }
-        /* [HttpGet]
-         public IEnumerable<LogSheetDTO> LogsheetByStaff(int id)
-
-         {
-             var result = _db.logsheets.Where(l => l.LogSheetId == id).ToList();
-             return (IEnumerable<LogSheetDTO>)result;
-         }*/
-
         [HttpGet]
-        public ActionResult<IEnumerable<LogDTO>> Get()
+        public IEnumerable<LogSheetDTO> LogsheetByStaff(int id)
+
         {
-            var logSheetDTOs = _db.logsheets.Select(logSheet => new LogDTO
-            {
-                LogSheetId = logSheet.LogSheetId,
-                UserId = logSheet.UserId,
-                StartTime = logSheet.StartTime,
-                EndTime = logSheet.EndTime,
-                LogType = logSheet.LogType,
-               // SubjectId = logSheet.SubjectId,
-               // CourseId = logSheet.CourseId,
-               // Topic = logSheet.Topic,
-               // verifiedBy = logSheet.verifiedBy,
-               // ApprovedBy = logSheet.ApprovedBy
-            }).ToList();
-
-            return logSheetDTOs;
+            var result = _db.LogSheets.Where(l => l.LogSheetId == id).ToList();
+            return result;
         }
-            
-            
-        
-
-
-
-
-            [HttpPost]
-        public bool AddLog([FromBody] LogSheetDTO logSheetDTO)
-        {
-            LogSheet log = new LogSheet();
-            log.LogSheetId = logSheetDTO.LogSheetId;
-            log.SubjectId = logSheetDTO.SubjectId;
-            log.CourseId = logSheetDTO.CourseId;
-            log.StartTime = logSheetDTO.StartTime;
-            log.EndTime = logSheetDTO.EndTime;
-            log.LogType = logSheetDTO.LogType;
-            log.Topic = logSheetDTO.Topic;
-            log.UserId  = logSheetDTO.UserId;
-            _db.logsheets.Add(log);
-            _db.SaveChanges();
-            return true;
-        }
-    }
 }
